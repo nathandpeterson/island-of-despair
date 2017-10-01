8fa52f6e5153ed14fd710fb694c1687b157cbbcc
Nathan Daniel Peterson <natperson@gmail.com>
2017-10-01 10:53:10 -0700

    improve design and layout

--- assets/scripts/dom.js
+++ assets/scripts/dom.js
@@ -1,7 +1,6 @@
 let healthDisplay = document.querySelector('#health')
 let foodDisplay = document.querySelector('#food')
 let statusDisplay = document.querySelector('#status')
-let discoverBtns = document.querySelectorAll('.discover')
 let actionBtns = document.querySelectorAll('.action')
 let dateDisplay = document.querySelector('#date')
 let tray = document.querySelector('.central-tray')
@@ -10,13 +9,8 @@ let dialogue = document.querySelector('#dialogue')
 for (let i = 0; i < actionBtns.length; i++){
   actionBtns[i].addEventListener('click', function(e){
     if(e.target.textContent.trim() === 'Hunt for Goats'){
-        find.goats();
+      find.goats();
     }
-  })
-}
-
-for (let i = 0; i < discoverBtns.length; i++){
-    discoverBtns[i].addEventListener('click', function(e){
     if(e.target.classList.contains('scavange-ship')) {
       exploreShip();
     }
